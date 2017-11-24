81d52e01f7120960748268f445900e5dbc681e7c
Connor Moran <moranconnorj@gmail.com>
2017-11-24 14:53:17 -0800

    added DisplayMessageActivity

--- MyFirstApp/app/src/main/java/com/example/connor/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/connor/myfirstapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.connor.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
+
